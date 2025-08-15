6849031b63b242ea8ab70d8256cee529ff796daa
brayanroa <brayanandresrl@ufps.edu.co>
2025-08-15 10:22:40 -0500

    feat(app.config): app.config and global styles modified

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,14 @@
 import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+
+// modules
+import { CoreModule } from './core/core-module';
+
+// primeNG 
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
-import { CoreModule } from './core/core-module';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,11 +17,15 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
 
     importProvidersFrom(CoreModule),
+
     // Asynchronous animations and PrimeNG configuration
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
-        preset: Aura
+        preset: Aura,
+        options: {
+          darkModeSelector: false  // ⚡ Desactiva detección automática de modo oscuro
+        }
       }
     })
   ]
